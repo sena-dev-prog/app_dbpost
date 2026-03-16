c16e9e48b412b9c55f86b7bb519c0041838764c4
sena-dev-prog <ficha.adsi.2026120@gmail.com>
2026-03-16 18:25:55 -0500

    carga_bd

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Construir la URL de conexión
-DB_URL = f"mysql+pymysql://prueba:prueba@localhost:3306/prueba"
+# DB_URL = f"mysql+pymysql://prueba:prueba@localhost:3306/prueba"
+DB_URL = f"mysql+pymysql://root:zFtBcyIKPGjwqcxYAuRvSSVCtYaZQNal@nozomi.proxy.rlwy.net:38890/railway"
 
 engine = create_engine(DB_URL)
 
